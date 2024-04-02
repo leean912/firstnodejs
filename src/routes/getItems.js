9f6cf91c755730075bac7789355adb6be472c00c
Lee An <lee.an@PeccaFile.com>
2024-04-02 16:21:25 +0800

    Dockerfile

--- src/routes/getItems.js
+++ src/routes/getItems.js
@@ -2,5 +2,9 @@ const db = require('../persistence');
 
 module.exports = async (req, res) => {
     const items = await db.getItems();
+    for (let i = 0; i < items.length; i++) {
+        console.log(`items: ${items[i]} | id: ${items[i]['id']} | name:  ${items[i]['name']} | completed: ${items[i]['completed']}`);
+    }
+
     res.send(items);
 };
